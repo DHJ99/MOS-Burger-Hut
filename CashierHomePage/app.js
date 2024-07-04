a3af44f92113f4a0ff6387b0846b9016d4baa4fc
DHJ99 <dananjayamadushanka825@gmail.com>
2024-07-04 08:33:18 +0530

    First

---
+++ CashierHomePage/app.js
@@ -0,0 +1,4 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+    document.getElementById('customerDetails').classList.remove('hidden');
+    // Here you would typically fetch customer data and populate the table
+});
