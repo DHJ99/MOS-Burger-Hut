05f4773a65c05f8a942954d159b0aec15d229427
DHJ99 <dananjayamadushanka825@gmail.com>
2024-07-30 16:02:45 +0530

    new 1

---
+++ OrderHistory/app.js
@@ -0,0 +1,20 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const rows = document.getElementById('customerTableBody').getElementsByTagName('tr');
+    
+    for (let i = 0; i < rows.length; i++) {
+        const phoneNumber = rows[i].getElementsByTagName('td')[2].textContent.toLowerCase();
+        if (phoneNumber.includes(searchInput)) {
+            rows[i].style.display = '';
+        } else {
+            rows[i].style.display = 'none';
+        }
+    }
+});
+
+window.onload = function() {
+    const rows = document.getElementById('customerTableBody').getElementsByTagName('tr');
+    for (let i = 0; i < rows.length; i++) {
+        rows[i].style.display = '';
+    }
+};
